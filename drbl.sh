4509c6e7338c2cd6f4fdbf3b3cc98d981c233d17
十百千 <x1001000@users.noreply.github.com>
2015-07-17 17:38:25 +0800

    Update drbl.sh

--- drbl.sh
+++ drbl.sh
@@ -1,24 +1,20 @@
-sudo -i
-wget -q http://drbl.nchc.org.tw/GPG-KEY-DRBL -O- | sudo apt-key add -
-echo deb http://free.nchc.org.tw/ubuntu trusty main restricted universe multiverse >> /etc/apt/sources.list
-echo deb http://free.nchc.org.tw/drbl-core drbl stable >> /etc/apt/sources.list
-apt-get update
-apt-get install drbl
-apt-get install xinit
+sudo wget -q http://drbl.nchc.org.tw/GPG-KEY-DRBL -O- | sudo apt-key add -
+sudo echo deb http://free.nchc.org.tw/ubuntu trusty main restricted universe multiverse >> /etc/apt/sources.list
+sudo echo deb http://free.nchc.org.tw/drbl-core drbl stable >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install drbl
+sudo apt-get install xinit
 
-echo auto eth0:1 >> /etc/network/interfaces
-echo iface eth0:1 inet static >> /etc/network/interfaces
-echo address 10.10.10.100 >> /etc/network/interfaces
-echo netmask 255.255.255.0 >> /etc/network/interfaces
-ifup eth0:1
+sudo echo auto eth0:1 >> /etc/network/interfaces
+sudo echo iface eth0:1 inet static >> /etc/network/interfaces
+sudo echo address 10.10.10.100 >> /etc/network/interfaces
+sudo echo netmask 255.255.255.0 >> /etc/network/interfaces
+sudo ifup eth0:1
 
-# 設定僅一次
+# only one time
 # drblsrv -i
-# 一直enter
 # drblpush -i
-# 一直enter
 
-# 要做派送
-# sudo dcs
-# 要進桌面
+# everytime
 # sudo startx
+# sudo dcs
