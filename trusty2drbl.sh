d79f7397ceb3a61eefbf7765183b49aff5301bac
十百千 <x1001000@users.noreply.github.com>
2015-07-23 19:55:38 +0800

    Update trusty2drbl.sh

--- trusty2drbl.sh
+++ trusty2drbl.sh
@@ -6,11 +6,12 @@ apt-get install drbl
 apt-get install lxde
 apt-get install xinit
 
-echo auto eth0:1 >> /etc/network/interfaces
-echo iface eth0:1 inet static >> /etc/network/interfaces
+IFACE=$(more /etc/network/interfaces | grep dhcp | cut -b 7-10)
+echo auto $IFACE:1 >> /etc/network/interfaces
+echo iface $IFACE:1 inet static >> /etc/network/interfaces
 echo address 10.10.10.100 >> /etc/network/interfaces
 echo netmask 255.255.255.0 >> /etc/network/interfaces
-ifup eth0:1
+ifup $IFACE:1
 
 echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | drblsrv -i
 echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | drblpush -i
